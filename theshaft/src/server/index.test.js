5430523b8b3e197c22ca4cc0306ccb735b33b363
Alex <alexwang3150@gmail.com>
2018-11-15 19:19:23 -0800

    fixing jest not ending

--- theshaft/src/server/index.test.js
+++ theshaft/src/server/index.test.js
@@ -3,17 +3,19 @@ const request = require('supertest');
 const os = require('os');
 
 describe("test the getUsername endpoint", () => {
+
     test('getUsername is successful', () => {
-        request(app).get('/api/getUsername').then((response) => {
+        expect.assertions(1);
+        return request(app).get('/api/getUsername').then((response) => {
             expect(response.statusCode).toBe(200);
-            done();
         });
     });
     test('getUsername returns correct username', () => {
-        request(app).get('/api/getUsername').then((response) => {
+        expect.assertions(1);
+        return request(app).get('/api/getUsername').then((response) => {
             console.log(response.body.username);
             expect(response.body.username).toBe(os.userInfo().username);
-            done();
         });
-    })
+    });
+
 });
