88530b2004488b23cff7fa686ac22b91e8a2111d
Alex <alexwang3150@gmail.com>
2018-11-15 19:27:58 -0800

    trying to fix jest again

--- theshaft/src/server/index.test.js
+++ theshaft/src/server/index.test.js
@@ -7,15 +7,15 @@ describe("test the getUsername endpoint", () => {
     test('getUsername is successful', () => {
         expect.assertions(1);
         return request(app).get('/api/getUsername').then((response) => {
-            expect(response.statusCode).toBe(200);
+            expect(response.statusCode).resolves.toBe(200);
         });
     });
-    
+
     test('getUsername returns correct username', () => {
         expect.assertions(1);
         return request(app).get('/api/getUsername').then((response) => {
             console.log(response.body.username);
-            expect(response.body.username).toBe(os.userInfo().username);
+            expect(response.body.username).resolves.toBe(os.userInfo().username);
         });
     });
 
