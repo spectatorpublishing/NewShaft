ad3af98bdb86ec44328578318f5b21c45cceb064
Matt <matthew.broughton@columbiaspectator.com>
2018-11-18 13:54:13 -0500

    temp commit

--- theshaft/src/server/index.js
+++ theshaft/src/server/index.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser')
 
 
 var getDormInfo = require('./routes/getDormInfo');
+var deleteDormInfo = require('./routes/deleteDormInfo');
 
 app.use(bodyParser.json())
 app.use(express.static('dist'));
 
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.use('/api/getDormInfo', (req, res) => getDormInfo(req, res) )
+app.use('/api/deleteDormInfo', (req, res) => deleteDormInfo(req, res) )
 app.listen(8080, () => console.log('Listening on port 8080!'));
 
 module.exports = app
