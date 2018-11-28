ae7549cc5400c021acaae63d2612000a94eb26d9
Matt <matthew.broughton@columbiaspectator.com>
2018-11-27 22:13:45 -0500

    photos comp finsih :D

--- theshaft/src/client/Photos.js
+++ theshaft/src/client/Photos.js
@@ -2,4 +2,19 @@ import React, { Component } from 'react';
 
 export default class Photos extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			image: this.props.image
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<img src={this.state.image} />
+			</div>
+		)
+	}
 }
