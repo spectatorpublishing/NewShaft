0d8b94852caf1ff6f43d36a36ca221c91a681104
nahum365 <nahum365@me.com>
2018-12-02 13:30:59 -0500

    React Router implemented

--- theshaft/src/client/index.js
+++ theshaft/src/client/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Explore from './containers/Explore';
+import Test from './containers/Test';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Explore</Link></li>
+        <li><Link to="/test">Test</Link></li>
+      </ul>
+      <Route exact path="/" component={Explore} />
+      <Route path="/test" component={Test} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
