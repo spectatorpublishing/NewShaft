cc78c26cc4deb85652400b81191aa03fbacb967c
Matt <matthew.broughton@columbiaspectator.com>
2018-12-02 13:47:30 -0500

    conflicts

--- theshaft/stories/index.js
+++ theshaft/stories/index.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 import SearchBar from '../src/client/components/SearchBar.js';
 import Photos from '../src/client/components/Photos.js';
+import DormButton from '../src/client/components/DormButton.js';
 
 storiesOf('Button', module)
   .add('with text', () => <Button>Hello Button</Button>)
@@ -14,8 +15,13 @@ storiesOf('Button', module)
     </Button>
   ));
 
+
 storiesOf('SearchBar', module)
   .add('with text', () => <SearchBar/>);
   
 storiesOf('Photos', module)
 	.add('pikachu', () => <Photos image="https://i.imgur.com/jevRP2j.jpg"/>);
+
+storiesOf('DormButton', module)
+	.add('dorm button', () => <DormButton name="McBANE?" address="70 Morningside Dr" sundial_distance="5000mi" description="a really great dorm who doesn't afraid of anything."/>)
+
