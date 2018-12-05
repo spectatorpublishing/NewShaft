22c3445d5ba082cf3c261e1687a943415762435d
Ray725@users.noreply.github.com <>
2018-12-04 21:02:56 -0500

    React testing works

--- theshaft/src/server/react.test.js
+++ theshaft/src/server/react.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Photos from '../client/components/Photos.js';
+import SearchBar from '../client/components/SearchBar.js';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('Photos renders correctly', () => {
   const tree = renderer
     .create(<Photos image="http://www.facebook.com"></Photos>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('SearchBar renders correctly', () => {
+  const tree = renderer
+    .create(<SearchBar/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
