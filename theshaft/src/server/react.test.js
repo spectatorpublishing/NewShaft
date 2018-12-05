746c82309e6e56f6b9611c723996ac9279d49d7f
Ray725@users.noreply.github.com <>
2018-12-04 21:58:40 -0500

    All react tests finished

--- theshaft/src/server/react.test.js
+++ theshaft/src/server/react.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Photos from '../client/components/Photos.js';
 import SearchBar from '../client/components/SearchBar.js';
+import DormButton from '../client/components/DormButton.js';
+import Explore from '../client/containers/Explore.js';
 import renderer from 'react-test-renderer';
 
 it('Photos renders correctly', () => {
@@ -13,6 +15,20 @@ it('Photos renders correctly', () => {
 it('SearchBar renders correctly', () => {
   const tree = renderer
     .create(<SearchBar/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('DormButton renders correctly', () => {
+  const tree = renderer
+    .create(<DormButton name="ADI House" address="21 Savage St." sundial_distance="12 minutes" description="It's lit"/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Explore renders correctly', () => {
+  const tree = renderer
+    .create(<Explore/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- theshaft/stories/index.js
+++ theshaft/stories/index.js
@@ -3,6 +3,8 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 import SearchBar from '../src/client/components/SearchBar.js';
 import Photos from '../src/client/components/Photos.js';
+import DormButton from '../src/client/components/DormButton.js';
+import Explore from '../src/client/containers/Explore.js';
 
 storiesOf('Button', module)
   .add('with text', () => <Button>Hello Button</Button>)
@@ -19,3 +21,9 @@ storiesOf('SearchBar', module)
   
 storiesOf('Photos', module)
 	.add('pikachu', () => <Photos image="https://i.imgur.com/jevRP2j.jpg"/>);
+
+storiesOf('DormButton', module)
+  .add('dorm button', () => <DormButton name="ADI House" address="21 Savage St." sundial_distance="12 minutes" description="It's lit"/>);
+
+storiesOf('Explore', module)
+  .add('explore', () => <Explore/>);
