7ef1d6f51ba4ee7a650dfab74402e2a26126753b
Arsalaan Ansari <aaa2325@columbia.edu>
2019-01-15 18:07:51 -0500

    Added maps + single marker funcitonality to Maps.js component. Added dotenv module to securely retrieve API keys, make sure to setup .env file in root directory with variable MAPBOX=api_key.

--- theshaft/src/client/containers/Dorm.js
+++ theshaft/src/client/containers/Dorm.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Photos from '../components/Photos';
+import Photos from "../components/Photos";
+import Maps from "../components/Maps";
+
 export default class Dorm extends React.PureComponent {
   render() {
     return (
       <div>
-        <Photos imageOne="https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg" imageTwo="https://i.imgflip.com/26a82h.jpg" imageThree="https://i.imgflip.com/1eg7jb.jpg" imageFour="https://i.imgflip.com/1yt82g.jpg"/>
+        <Photos
+          imageOne="https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+          imageTwo="https://i.imgflip.com/26a82h.jpg"
+          imageThree="https://i.imgflip.com/1eg7jb.jpg"
+          imageFour="https://i.imgflip.com/1yt82g.jpg"
+        />
         <h1>{this.props.match.params.dorm}</h1>
+        <Maps />
         <Link to="/">Back</Link>
       </div>
     );
