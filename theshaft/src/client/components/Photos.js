c9d92e3d5ce1757519b607bd79dc7c3796c7a8f2
Irene Nam <yn2334@columbia.edu>
2019-01-27 17:03:19 -0500

    div changes for the photos, still in the works

--- theshaft/src/client/components/Photos.js
+++ theshaft/src/client/components/Photos.js
@@ -1,5 +1,54 @@
 import React, { Component } from "react";
-import "../css/Photos.css";
+import styled from 'styled-components';
+
+let Imagecontainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 40vh;
+  width: 100%;
+  overflow: hidden;
+  align-items: center;
+`
+
+let PicsBig = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 66%;
+  height: 100%;
+  overflow: hidden;
+  align-items: center;
+`
+
+let PicsMid = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 33%;
+  overflow: hidden;
+`
+
+let PicsLow = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 50%;
+  overflow: hidden;
+`
+
+let Pinky = styled.img`
+  height: 50%;
+`
+
+let Inky = styled.img`
+  height: 100%;
+  width: 50%;
+`
+
+let Clyde = styled.img`
+  height: 100%;
+  width: 50%;
+`
+
 
 export default class Photos extends Component {
   constructor(props) {
@@ -15,16 +64,18 @@ export default class Photos extends Component {
 
   render() {
     return (
-      <div class="picsHigh">
-        <img class="blinky" src={this.state.imageOne} />
-        <div class="picsMid">
-          <img class="pinky" src={this.state.imageTwo} />
-          <div class="picsLow">
-            <img class="inky" src={this.state.imageThree} />
-            <img class="clyde" src={this.state.imageFour} />
-          </div>
-        </div>
-      </div>
+      <Imagecontainer>
+        <PicsBig>
+          <img src={this.state.imageOne} />
+        </PicsBig>
+        <PicsMid>
+          <Pinky src={this.state.imageTwo} />
+          <PicsLow>
+            <Inky src={this.state.imageThree} />
+            <Clyde src={this.state.imageFour} />
+          </PicsLow>
+        </PicsMid> 
+      </Imagecontainer>
     );
   }
 }
