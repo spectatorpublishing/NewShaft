438030266b5fa9deb45b496c60bc2b58f3abde6b
Ray725@users.noreply.github.com <>
2019-01-27 17:00:45 -0500

    asdf

--- theshaft/stories/index.js
+++ theshaft/stories/index.js
@@ -50,6 +50,8 @@ storiesOf('FloorPlan', module)
 storiesOf('Expander', module)
   .add('expander', () => <Expander showAll="Here's all of the text shown. It should be longer than the preview." showSome="Here's a preview shown."><h1>Some Static Heading</h1></Expander>);
 
+storiesOf('test', module)
+  .add('test', () => <Expander showAll={<Reviews/>} showSome="Here's a preview shown."><h1>Some Static Heading</h1></Expander>);
 
 let sampleAmenities = [
   ["bathroom", "Semi-private"],
