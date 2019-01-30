250cf04326c2c8ca23e1ef820c22df5fb3e970ff
Serena Cheng <serenacheng99@gmail.com>
2019-01-29 21:25:34 -0500

    add multiple map markers, but popups are not synced correctly

--- theshaft/src/client/components/Maps.js
+++ theshaft/src/client/components/Maps.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import ReactMapGL, { Marker, Popup} from "react-map-gl";
 import { fromJS } from "immutable";
 import "mapbox-gl/src/css/mapbox-gl.css";
 import mark from "../assets/marker.svg";
-import "../css/Maps.css";
+import "./Maps.css";
 
 console.log("Entry");
 console.log(process.env)
@@ -12,6 +12,7 @@ console.log(process.env.MAPBOX);
 export default class Maps extends Component {
   constructor(props) {
     super(props);
+    var popupIndex = this.props.popupInfo.map(() => {return false})
     this.state = {
       viewport: {
         width: 400,
@@ -19,16 +20,74 @@ export default class Maps extends Component {
         latitude: 40.7128,
         longitude: -74.006,
         zoom: 15
+      },
+      coordinates: {
+        latitudes: this.props.latitudes,
+        longitudes : this.props.longitudes
+      },
+      popup: {
+        popupInfo: this.props.popupInfo,
+        popupIndex: popupIndex
       }
     };
   }
 
+  renderPopup(){
+    console.log("Rendering! popups: " + this.state.popup.popupIndex);
+    var popups = this.state.popup.popupIndex.map((e, i) => {
+      if(e){
+        return (
+          <Popup tipSize={10}
+            anchor="bottom-right"
+            longitude={this.state.coordinates.longitudes[i]}
+            latitude={this.state.coordinates.latitudes[i]}
+            onClose={() => {
+              var popupIndex = this.state.popup.popupIndex;
+              popupIndex[i] = false;
+              console.log("Closed! popups: " + popupIndex)
+              this.setState({popup: {popupInfo: this.state.popup.popupInfo, popupIndex: popupIndex}})}
+            }
+            closeOnClick={true}>
+            <p>{this.state.popup.popupInfo[i]}</p>
+          </Popup>
+        );
+      }
+    });
+
+    return popups;
+  }
+
   render() {
     const view = this.state.viewport;
+    
+    var markers = this.state.coordinates.latitudes.map((lat, i) => {
+      var long = this.state.coordinates.longitudes[i];
+      var handlePopupClick = function () {
+        var popupIndex = this.state.popup.popupIndex;
+        popupIndex[i] = true;
+        console.log("Clicked! popups: " + popupIndex)
+        this.setState({popup: {popupInfo: this.state.popup.popupInfo, popupIndex: popupIndex}});
+      }
+      var marker =  <Marker
+                      latitude={lat}
+                      longitude={long}
+                      offsetLeft={-20}
+                      offsetTop={10}
+                    >
+                      <div>{"Marker" + i}</div>
+                      <div onClick={() => {handlePopupClick()}}>
+                        <img src={mark} className="marker_style" alt="fireSpot"/>
+                      </div>
+                    </Marker>
+      // handlePopupClick = handlePopupClick.bind(marker)
+      return marker;
+    });
+    
     return (
       <div>
         <ReactMapGL
-          mapboxApiAccessToken={process.env.MAPBOX}
+          // mapboxApiAccessToken={process.env.MAPBOX}
+          mapboxApiAccessToken={"pk.eyJ1IjoiYXJzYWxhYW4iLCJhIjoiY2pxeDViZW41MDlmejQ4bnduMnE2aGhyNCJ9.0-y9yPqzqlWLd-yhUe5tcg"}
           mapStyle={"mapbox://styles/mapbox/basic-v9"}
           latitude={view.latitude}
           longitude={view.longitude}
@@ -37,16 +96,11 @@ export default class Maps extends Component {
           zoom={view.zoom}
           onViewportChange={viewport => this.setState({ viewport })}
         >
-          <Marker
-            latitude={40.7128}
-            longitude={-74.006}
-            offsetLeft={-20}
-            offsetTop={-10}
-          >
-            <img src={mark} className="marker_style" alt="fireSpot" />
-          </Marker>
+        {markers}
+        {this.renderPopup()}
         </ReactMapGL>
       </div>
     );
   }
 }
+
