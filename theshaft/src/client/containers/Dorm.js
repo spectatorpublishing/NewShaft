f876f3e68e9601b8427dc7e0c7cd016970f37d80
Arsalaan Ansari <aaa2325@columbia.edu>
2019-01-30 23:39:54 -0500

    Dumped all relevant components on the Dorm.js page.

--- theshaft/src/client/containers/Dorm.js
+++ theshaft/src/client/containers/Dorm.js
@@ -6,6 +6,11 @@ import Amenities from "../components/Amenities";
 import AtAGlance from "../components/AtAGlance";
 import Maps from "../components/Maps";
 import ProCon from "../components/ProCon";
+import FloorPlan from "../components/FloorPlan";
+import Reviews from "../components/Reviews";
+import RelatedDorms from "../components/RelatedDorms";
+
+
 
 let sampleAmenities = [
   ["bathroom", "Semi-private"],
@@ -17,6 +22,17 @@ let sampleAmenities = [
   ["lounge", "Sky lounge"],
   ["lounge", "Basement lounge"]
 ];
+
+let sampleRelatedDorms = [
+  ["Carman", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
+  ["Furnald", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
+  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
+  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
+  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
+  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"]
+];
+
+
 
 const testPros = ["pro1", "pro2", "pro3"];
 const testCons = ["con1", "con2", "con3"];
@@ -90,7 +106,9 @@ export default class Dorm extends React.PureComponent {
             <Amenities amenities={sampleAmenities}/>
             <Maps/>
             <ProCon pros={testPros} cons={testCons}></ProCon>
-            <Link to="/">Back</Link>
+            <FloorPlan floorOffset={1} planArray={["https://housing.columbia.edu/files/housing/Wien%208_2018.jpg", "https://housing.columbia.edu/files/housing/Wien%208_2018.jpg","https://housing.columbia.edu/files/housing/600%209_2016_0.jpg","https://housing.columbia.edu/files/housing/Woodbridge%204_2018.jpg", "https://i.kym-cdn.com/entries/icons/original/000/026/642/kot1.jpg"]}/>
+            <Reviews/>
+            <RelatedDorms relatedDorms={sampleRelatedDorms}/>
           </ColTwo>
           <ColThree>
             <AtAGlance location="545 W. 114th St." roomtype="Suite-style doubles" classmakeup="First-Years" numfloors="13"/>
