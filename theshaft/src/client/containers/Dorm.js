67d92df6cfbc9418619c9c8a05a63d89bf80e074
Arsalaan Ansari <aaa2325@columbia.edu>
2019-02-05 18:45:33 -0500

    Fixed alignment of at a glance component and spacing between blocks.

--- theshaft/src/client/containers/Dorm.js
+++ theshaft/src/client/containers/Dorm.js
@@ -7,10 +7,8 @@ import AtAGlance from "../components/AtAGlance";
 import Maps from "../components/Maps";
 import ProCon from "../components/ProCon";
 import FloorPlan from "../components/FloorPlan";
-import Reviews from "../components/Reviews";
+import FullReview from "../components/FullReview";
 import RelatedDorms from "../components/RelatedDorms";
-
-
 
 let sampleAmenities = [
   ["bathroom", "Semi-private"],
@@ -24,15 +22,31 @@ let sampleAmenities = [
 ];
 
 let sampleRelatedDorms = [
-  ["Carman", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
-  ["Furnald", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
-  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
-  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
-  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"],
-  ["John Jay", "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"]
+  [
+    "Carman",
+    "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+  ],
+  [
+    "Furnald",
+    "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+  ],
+  [
+    "John Jay",
+    "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+  ],
+  [
+    "John Jay",
+    "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+  ],
+  [
+    "John Jay",
+    "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+  ],
+  [
+    "John Jay",
+    "https://memegenerator.net/img/images/17438601/dat-sad-fat-cat.jpg"
+  ]
 ];
-
-
 
 const testPros = ["pro1", "pro2", "pro3"];
 const testCons = ["con1", "con2", "con3"];
@@ -43,10 +57,10 @@ let Header = styled.div`
   position: relative;
   top: -140px;
   margin-left: 15vw;
-`
+`;
 
 let Blurb = styled.div`
-  background-color: #44A7FF;
+  background-color: #44a7ff;
   color: white;
   font-size: 0.8rem;
   font-weight: 300;
@@ -57,38 +71,40 @@ let Blurb = styled.div`
   padding: 1.8vw;
   border-radius: 1.5vw;
   width: 70vw;
-`
+`;
 
 let Body = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
-`
+`;
 
 let ColOne = styled.div`
   display: flex;
   width: 33%;
-`
+`;
 
 let ColTwo = styled.div`
   display: flex;
   flex-direction: column;
   width: 50%;
   margin-top: 50px;
-`
+`;
 
 let ColThree = styled.div`
   display: flex;
+  flex-direction: column;
   width: 33%;
-`
+`;
 
-let DormContainer = styled.div`
-`
+let DormContainer = styled.div``;
 
 let AtAGlanceContainer = styled.div`
   margin-left: 50px;
-`
+`;
 
+let Block = styled.div`
+  margin-bottom: 25px;
+`;
 
 export default class Dorm extends React.PureComponent {
   render() {
@@ -102,19 +118,48 @@ export default class Dorm extends React.PureComponent {
           imageFive="https://arc-anglerfish-arc2-prod-spectator.s3.amazonaws.com/public/52FBXLYM2RGO3FJGK3SPD2KUEE.png"
         />
         <Header>{this.props.match.params.dorm}</Header>
-        <Blurb>This is a blurb for the dorm summary. This is just a test. Blah bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.  <br/> Hi <br/> Bye</Blurb>
+        <Blurb>
+          This is a blurb for the dorm summary. This is just a test. Blah bla
+          bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla.{" "}
+          <br /> Hi <br /> Bye
+        </Blurb>
+
         <Body>
-          <ColOne/>
+          <ColOne />
           <ColTwo>
-            <Amenities amenities={sampleAmenities}/>
-            <ProCon pros={testPros} cons={testCons}></ProCon>
-            <FloorPlan floorOffset={1} planArray={["https://housing.columbia.edu/files/housing/Wien%208_2018.jpg", "https://housing.columbia.edu/files/housing/Wien%208_2018.jpg","https://housing.columbia.edu/files/housing/600%209_2016_0.jpg","https://housing.columbia.edu/files/housing/Woodbridge%204_2018.jpg", "https://i.kym-cdn.com/entries/icons/original/000/026/642/kot1.jpg"]}/>
-            <Reviews/>
-            <RelatedDorms relatedDorms={sampleRelatedDorms}/>
+            <Block>
+              <Amenities amenities={sampleAmenities} />
+            </Block>
+            <Block>
+              <ProCon pros={testPros} cons={testCons} />
+            </Block>
+            <Block>
+              <FloorPlan
+                floorOffset={1}
+                planArray={[
+                  "https://housing.columbia.edu/files/housing/Wien%208_2018.jpg",
+                  "https://housing.columbia.edu/files/housing/Wien%208_2018.jpg",
+                  "https://housing.columbia.edu/files/housing/600%209_2016_0.jpg",
+                  "https://housing.columbia.edu/files/housing/Woodbridge%204_2018.jpg",
+                  "https://i.kym-cdn.com/entries/icons/original/000/026/642/kot1.jpg"
+                ]}
+              />
+            </Block>
+            <Block>
+              <FullReview />
+            </Block>
+            <Block>
+              <RelatedDorms relatedDorms={sampleRelatedDorms} />
+            </Block>
           </ColTwo>
           <ColThree>
             <AtAGlanceContainer>
-              <AtAGlance location="545 W. 114th St." roomtype="Suite-style doubles" classmakeup="First-Years" numfloors="13"/>
+              <AtAGlance
+                location="545 W. 114th St."
+                roomtype="Suite-style doubles"
+                classmakeup="First-Years"
+                numfloors="13"
+              />
             </AtAGlanceContainer>
           </ColThree>
         </Body>
