dd62c20989848ef90088daeccce6912e8d00446b
Irene Nam <yn2334@columbia.edu>
2019-02-05 20:55:00 -0500

    related dorms

--- theshaft/src/client/components/RelatedDorms.js
+++ theshaft/src/client/components/RelatedDorms.js
@@ -1,18 +1,47 @@
 
 /*
     RelatedDorms component.
-
     This component displays the related dorms of a given
     dorm page. The names and image sources for this component
     are to be passed into the component as an array.
-
 */
 
 import React, { Component } from "react";
-import "../css/RelatedDorms.css";
 import styled from "styled-components";
 //var element = document.relatedDormsList.style;
 //element.setPropety('--mw', '800px');
+
+let RelatedDormsList = styled.div`
+    /* border: 1px black solid;
+    border-radius: 1px;  */
+    width: 100%
+    max-height: 40vw;
+    height: 30vw;
+`
+
+let RelatedDormsTitle = styled.div`
+    color: grey;
+    font-weight: bolder;
+    font-size: 2vw;
+    margin: 4vw 0vw 1vw 0.5vw;
+`
+
+let RelatedDormsHorizontalView = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap
+`
+
+let RelatedDormImage = styled.img`
+    height: 6.5vw;
+    width: 6.5vw;
+    margin: 0.3vw 0.3vw 0.3vw 0.3vw;
+`
+
+let RelatedDormName = styled.div`
+    margin: 0px 0 0 1.3vw;
+    color: grey;
+`
 
 
 export default class RelatedDorms extends Component {
@@ -35,7 +64,6 @@ export default class RelatedDorms extends Component {
         prop that works as a two-index array. The title is passed
         from the [0] index and image source is passed from [0] of
         the property. 
-
         !!! ALERT !!!
         The title of the dorm needs to be pulled from a Title Component.
         */
@@ -59,6 +87,5 @@ export default class RelatedDorms extends Component {
                 </RelatedDormsHorizontalView>
             </RelatedDormsList>
         );
-
     }
 }
