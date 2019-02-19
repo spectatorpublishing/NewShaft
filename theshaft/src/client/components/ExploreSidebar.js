1fbadb653b58f03c8802d46373e11b8cea9797a2
ArsalaanAnsari <aaa2325@columbia.edu>
2019-02-19 17:35:59 -0500

    Revert "Revert "Dynamic sidebar""

--- theshaft/src/client/components/ExploreSidebar.js
+++ theshaft/src/client/components/ExploreSidebar.js
@@ -1,29 +1,61 @@
 import React, { Component } from 'react';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import Filter from './FilterComponent.js'
 
 import DormButton from '../components/DormButton';
 import '../css/ExploreSidebar.css';
 
 export default class ExploreSidebar extends Component {
-
     constructor(props) {
         super(props);
-        this.state = {dorms: props.dorms};
+        this.state = {
+            payload: this.props.payload,
+            dorms: [],
+            dormButtons: []
+        };
+    }
+
+    fetchDorms(){
+        // console.log("PAYLOAD: " + JSON.stringify(this.state.payload))
+        fetch('/api/filterDorm', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.payload)
+        }).then(res => res.json())
+        .then(response => {
+            // console.log("RESPONSE: " + JSON.stringify(response))
+            this.setState({dorms: response})
+        });      
+    }
+
+    componentDidMount(){
+        this.fetchDorms();
+    }
+
+    componentDidUpdate(prevProps){
+        if(prevProps != this.props){
+            console.log("props updated")
+            this.setState({payload: this.props.payload}, () => this.fetchDorms()); 
+        }      
     }
 
     render() {
         var dormsList = this.state.dorms.map((dorm, index) => (<Link key={index} to={"/" + dorm.id} style={{ textDecoration: 'none' }}><DormButton key={index} school={dorm.school} name={dorm.name} image={dorm.image} description={dorm.description} amenities={dorm.amenities}/></Link>));
         return (
             <div>
-                
-                <div className="filters">
-                    <h2>The Shaft</h2>
-                    <Filter/>
-                </div>
                 <hr className="sidebar-divider"/>
                 <div className="dorms">
-                    {dormsList}
+                    {this.state.dorms.map((dorm, index) => 
+                        <Link key={index} to={"/" + dorm.dorm} style={{textDecoration: 'none'}}>
+                            <DormButton key={index}
+                                school={dorm.college}
+                                name={dorm.dorm + ' Hall'}
+                                image={dorm.thumbnail_image}
+                                description={dorm.description}
+                                amenities={dorm.amenities}/>
+                        </Link>
+                    )}
                 </div>
             </div>
         );
