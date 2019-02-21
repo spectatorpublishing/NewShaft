f2dce712520c53cc55224c13565d90ece850fab0
nahum365 <nahum365@me.com>
2019-02-20 22:26:18 -0500

    Fix json scraper, updated json

---
+++ theshaft/scripts/lotterynumber_scraper.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var allDorms = {};
+
+request('https://housing.columbia.edu/room-selection/tips-and-tools/resources', function (error, response, html) 
+{
+    var $ = cheerio.load(html);
+    $('tr').each(function(i, element){
+        if (i == 0)
+            return true;
+        var data = $(this).children();
+        var title = $(data).eq(0).text().split("-");
+        var dorm = title[0].trim();
+        var suiteType = title[1].trim();
+        var cutoff1 = $(data).eq(1).text();
+        var cutoff2 = $(data).eq(2).text();
+
+        if (!(dorm in allDorms))
+        {
+            allDorms[dorm] = {};
+        }
+        allDorms[dorm][suiteType] = cutoff2;
+      });
+      console.log(JSON.stringify(allDorms));
+});
+
