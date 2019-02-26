66871fd2d6ac8ab8476693b76e52a11e1b367951
evookelj <evukelj@stuy.edu>
2019-02-25 23:48:21 -0500

    Write python script for new sql server

--- theshaft/db/insert_script.sh
+++ theshaft/db/insert_script.sh
@@ -17,11 +17,11 @@ while read LINE; do
     fi 
     
     if [ $first_line -ne 0 ]
-       #then echo "INSERT INTO ${table} VALUES (${LINE});"
+       # then echo "INSERT INTO ${table} (${cols}) VALUES (${LINE});"
        then sql_query="${sql_query} INSERT INTO ${table} (${cols}) VALUES (${LINE});"
     fi
     first_line=1 
 done < $FILE
 
 # actually insert to sql!
-mysql -u spectech -p -h 85.10.205.173 --database="theshaft" --execute "${sql_query}"
+mysql -u root -p -h 157.230.66.55 --database="dorms" --execute "${sql_query}"
