14953ed71818594cbde2f649320e4d92a0249c2b
Rounak Bera <rounakbera@gmail.com>
2019-02-27 22:39:42 -0500

    spectrum sidebar styled/ in dorm.js with dummy data

---
+++ theshaft/src/client/components/SpectrumSidebar.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const SmallWrapper = styled.div `
+	width: 100%;
+	display: flex;
+	margin-bottom: 15px;
+`
+const ImageDiv = styled.div `
+	width: 40%;
+	margin-right: 15px;
+`
+
+const Image = styled.img `
+	width: 100%;
+	height: 200px;
+`
+const Title = styled.p `
+	margin-top: -3px;
+`
+
+const AuthorLine = styled.p `
+    font-size: 12px;
+    color: grey;
+  `
+
+const Wrapper = styled.div `
+	width: 100%;
+`
+
+const SectionHeader = styled.div `
+  border-radius: 3px;
+  background: #00BFFF;
+  text-align: center;
+  font-size: 20px;
+  color: white;
+  vertical-align: middle;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-right: 10px;
+  padding-left: 10px;
+`
+
+const Logo = styled.img `
+	display: block;
+  	margin-left: auto;
+  	margin-right: auto;
+  	margin-bottom: 50px;
+  	width: 30%;
+`
+
+class SidebarItem extends Component {
+    render(){
+    	return (
+	        <SmallWrapper>
+				<ImageDiv>
+					<Image src={this.props.img_src}/>
+				</ImageDiv>
+				<div style={{width: "70%"}}>
+					<Title><h3>{this.props.title}</h3></Title>
+					<hr/>
+					<AuthorLine><p>{this.props.author}</p><p>{this.props.date}</p></AuthorLine>
+				</div>
+			</SmallWrapper>
+    	);
+    }
+}
+
+class SpectrumSidebar extends Component {
+
+	articleMap(){
+		const articleData = this.props.spectrumSidebarData;
+		return (
+			articleData.map(data =>{
+					return <SidebarItem 
+						title={data.title} 
+						img_src={data.img_src} 
+						author = {data.author}
+						date = {data.date}/>
+				}
+			)
+		)
+	}
+
+    render(){
+        return(
+	        <Wrapper>
+				<Logo src="https://www.columbiaspectator.com/pb/resources/img/spectrum-logo-NEW.png"/>
+				{this.articleMap()}
+			</Wrapper>
+		);
+	}
+}
+
+export default SpectrumSidebar
--- theshaft/stories/index.js
+++ theshaft/stories/index.js
@@ -28,6 +28,27 @@ import FloorPlanSVG from '../src/client/components/FloorPlanSVG.js'
 import { ReactComponent as SymposiumSVG } from "../src/client/assets/test_floorplan.svg";
 // import ReviewSlider from "../src/client/components/ReviewSlider.js";
 import SlidingReview from "../src/client/components/SlidingReview.js";
+import SpectrumSidebar from "../src/client/components/SpectrumSidebar.js";
+
+storiesOf('SpectrumSidebar', module)
+  .add('SpectrumSidebar', () =>
+    <SpectrumSidebar
+                spectrumSidebarData = {[
+                  {
+                    title: "How Have Local Hiring Targets Shaped Columbia’s Manhattanville Construction Site?", 
+                    img_src: "https://www.gstatic.com/webp/gallery/1.jpg", 
+                    author: "BY YULONG LI",
+                    date: "APRIL 8, 2018"
+                  },
+                  {
+                    title: "Newly proposed committee for Barnard calls for increased transparency", 
+                    img_src: "https://www.gstatic.com/webp/gallery/3.jpg", 
+                    author: "BY ROUNAK",
+                    date: "APRIL 7, 2018"
+                  }
+                ]}
+    />
+    );
 
 // storiesOf('ReviewSlider', module)
 //   .add('test', () => <ReviewSlider />);
