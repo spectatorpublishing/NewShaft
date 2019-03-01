eda6db6aa85387cbe60b0393d81b377701669f0c
nahum365 <nahum365@me.com>
2019-03-01 05:52:14 -0500

    Reviews fixes

--- theshaft/src/server/routes/getReviews.js
+++ theshaft/src/server/routes/getReviews.js
@@ -38,6 +38,7 @@ function getReviews(con, request, callback) {
 			}
 			var avg_rating =  (avg_rating / res.length).toFixed(1);
 			con.query(sqlStatement2, function(err, res2) {
+				var reccomended = Object.values(res2[0])[0].toFixed(1) * 100 + "%";
 				con.query(sqlStatement3, function(err, res3) {
 					var ranking = "-"
 					for (var i = 0; i < res3.length; i++) {
@@ -45,7 +46,7 @@ function getReviews(con, request, callback) {
 							var ranking = res3[i]["row_number"];
 						}
 					}
-					var to_return = {reccomended: Object.values(res2[0])[0] * 100 + "%", avg_rating: avg_rating, ranking: ranking, reviews: res};
+					var to_return = {reccomended: reccomended, avg_rating: avg_rating, ranking: ranking, reviews: res};
 					callback(to_return);
 				});
 				con.end(); // DO NOT REMOVE!
