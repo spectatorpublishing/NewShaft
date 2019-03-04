ce4e8a38835b2d49db35477cc905a725f4c5dad5
Arsalaan Ansari <aaa2325@columbia.edu>
2019-03-04 13:38:33 -0500

    Fixed minor ad issue

--- theshaft/src/client/components/AdManager.js
+++ theshaft/src/client/components/AdManager.js
@@ -18,7 +18,7 @@ export default class Example extends Component {
         <div>
           <DFPSlotsProvider dfpNetworkId="59699124">
             <div className="desktop-ads">
-              <AdSlot sizes={[[728, 90]]} adUnit="cds_leaderboard" />
+              <AdSlot sizes={[[728, 90]]} adUnit="shaftleader" />
             </div>
           </DFPSlotsProvider>
           </div>
