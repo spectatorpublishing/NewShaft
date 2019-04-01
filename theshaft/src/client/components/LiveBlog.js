d56d492e390de2af0073cfe15414f72fc085c4ad
Arsalaan Ansari <aaa2325@columbia.edu>
2019-03-31 22:20:27 -0400

    Added liveblog component

---
+++ theshaft/src/client/components/LiveBlog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+export default class LiveBlog extends Component {
+  render() {
+    
+      return (
+        <div>
+         
+          </div>
+        );
+
+    }
+    
+  }
+}
