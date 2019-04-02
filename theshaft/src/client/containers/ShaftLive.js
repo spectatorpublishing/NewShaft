c72b565d52940a96f6a0fc2b2d3a869fff6e0073
Arsalaan Ansari <aaa2325@columbia.edu>
2019-04-02 02:31:11 -0400

    Default 47 Claremont

--- theshaft/src/client/containers/ShaftLive.js
+++ theshaft/src/client/containers/ShaftLive.js
@@ -108,88 +108,7 @@ let FloorPlanLegend = styled.div`
     margin-bottom: 1rem;
 `
 
-let floorplanData = [
-    {
-      "DORM": "47 Claremont",
-        "ROOM": "4",
-        "FLOOR": "1",
-        "ROOM_TYPE": "suite-3",
-        "NEW_PRIORITY": "30",
-        "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-        "ROOM": "3",
-        "FLOOR": "1",
-        "ROOM_TYPE": "suite-4",
-        "NEW_PRIORITY": "30",
-        "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-        "ROOM": "1",
-        "FLOOR": "1",
-        "ROOM_TYPE": "suite-6",
-        "NEW_PRIORITY": "30",
-        "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-      "ROOM": "2",
-      "FLOOR": "1",
-      "ROOM_TYPE": "suite-5",
-      "NEW_PRIORITY": "30",
-      "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-        "ROOM": "24",
-        "FLOOR": "2",
-        "ROOM_TYPE": "suite-3",
-        "NEW_PRIORITY": "30",
-        "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-        "ROOM": "23",
-        "FLOOR": "2",
-        "ROOM_TYPE": "suite-4",
-        "NEW_PRIORITY": "30",
-        "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-        "ROOM": "21",
-        "FLOOR": "2",
-        "ROOM_TYPE": "suite-7",
-        "NEW_PRIORITY": "30",
-        "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "47 Claremont",
-      "ROOM": "22",
-      "FLOOR": "2",
-      "ROOM_TYPE": "suite-7",
-      "NEW_PRIORITY": "30",
-      "NEW_NUM": "3000"
-    },
-    {
-      "DORM": "River Hall",
-      "ROOM": "606",
-      "FLOOR": "6",
-      "ROOM_TYPE": "single",
-      "NEW_PRIORITY": "",
-      "NEW_NUM": ""
-    },
-    {
-      "DORM": "River Hall",
-      "ROOM": "607",
-      "FLOOR": "6",
-      "ROOM_TYPE": "single",
-      "NEW_PRIORITY": "20",
-      "NEW_NUM": "1234"
-    }
-  ];
+
 
 
 export default class ShaftLive extends Component {
@@ -197,9 +116,9 @@ export default class ShaftLive extends Component {
         super(props);
 
         this.state = {
-            dorm: "Broadway Hall",
+            dorm: "47 Claremont",
             dormRefresh: false,
-            floor: "3",
+            floor: "1",
             floorNums: null,
             floorData: [],
             width: window.innerWidth,
