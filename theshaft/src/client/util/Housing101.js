b2e178fa79f60f49c69a8edc5450a803b21e786e
Arsalaan Ansari <aaa2325@columbia.edu>
2019-04-02 02:20:30 -0400

    Added prelim json for Housing101

---
+++ theshaft/src/client/util/Housing101.js
@@ -0,0 +1,26 @@
+const housing = [
+    {
+        "title": "Welcome to theSHAFT",
+        "content": [
+            {
+                "subtitle": "What is 'the shaft'",
+                "body": "We’re not sure that the McBain shaft exists for any purpose other than to remind you every time you enter your room that you lost the housing lottery. Located within the vertical passages in and between buildings, shafted rooms, which can be found in McBain, Broadway, Woodbridge, and 47 Claremont, are usually hot, noisy and dark, although one perk is that they usually have air conditioning, which can be a game changer."
+
+            }
+           
+        ]
+    },
+    {
+        "title": "Housing Registration Basics",
+        "content": [
+            {
+                "subtitle": "Group vs. Individual Selection",
+                "body": "Groups consist of two to eight people and must be decided upon when you register. The recommended deadline to form and verify a group for Columbia students was Wednesday, March 6. Once groups are formed, they can only be broken, not expanded, with the exception of senior/junior regroup. In-person room selection occurs between April 2 and April 8 this year and is only available for groups. Individuals, along with groups that don’t choose during in-person selection, choose rooms through online room selection in the second half of April. "
+            },
+            {
+                "subtitle": "Difference between in-person and online room selection",
+                "body": "If you’re planning on living in a double or suite next semester, your group will be invited to John Jay Lounge (aka “The Cage”) sometime between April 2 and April 8 based on your lottery number. (Read this article to learn more about the room selection timeline.) All group members must be present or send proxies to select rooms; proxies must be made during the registration application before the appointment. The suites picked must match the size of your group, though during in-person selection, rising sophomores in groups have the additional option of pairing off and selecting doubles in corridor-style halls like McBain or Wien. &#10; If you’re in a group and can’t make your in-person appointment, you can also choose to drop to online selection. Although your group will retain its lottery number, be aware that some of the more popular rooms might be gone by the time online registration rolls around. &#10; If you’re applying as an individual or aren’t able to select a room with your group, you’ll be automatically given an online registration appointment sometime between April 15 and 30. During your appointment slot, you’ll be able to select singles, half-filled doubles, or full-doubles. If you were originally in a group that wasn’t able to secure a suite, online registration is a great opportunity to find rooms on the same floor so you can still be near your friends next semester."
+            }
+        ]
+    }
+]
