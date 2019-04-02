1ecbeb58ae5c8f0928b02a586e4d3aa6599f9772
Kevin Li <kevinl94303spam@gmail.com>
2019-04-02 05:09:52 -0400

    pipe character lotterty nums

--- theshaft/src/client/components/FloorPlanSVG.js
+++ theshaft/src/client/components/FloorPlanSVG.js
@@ -322,13 +322,13 @@ export default class FloorPlanSVG extends Component {
       // Taken room (Red)
       if (fromDb["NEW_PRIORITY"]) {
         lotteryLabel = "Taken By";
-        lottery = fromDb["NEW_PRIORITY"] + " | " + fromDb["NEW_NUM"];
+        lottery = fromDb["NEW_PRIORITY"] + "/" + fromDb["NEW_NUM"];
       }
   
       return <TooltipBox>
         <TooltipText>{label}: <TooltipBold>{room}</TooltipBold></TooltipText>
         <TooltipText>
-          {lotteryLabel}: <TooltipBold>{lottery}</TooltipBold>
+          {lotteryLabel}: <TooltipBold>{lottery.replace('/', ' | ')}</TooltipBold>
         </TooltipText>
         <TooltipText>
         {roomTypeLabel}: <TooltipBold>{roomType}</TooltipBold>
