acfe6154250c2081fc1d0275457b87a0052ad7b0
Yuanyuting Wang <yw3241@columbia.edu>
2020-04-03 15:02:52 -0400

    fixed sql syntax error

--- src/server/routes/getMoreDormInfo.js
+++ src/server/routes/getMoreDormInfo.js
@@ -4,13 +4,16 @@ var pool = require('../database');
 
 router.get('/:dorm', async (req, res) => {
 
-      let query = `SELECT dorm_static_info.DORM, dorm_static_info.WALKTHROUGH, dorm_static_info.SUITE, dorm_static_info.SINGLE_, dorm_static_info.DOUBLE_, dorm_static_info.TRIPLE_, dorm_static_info.CLASS_MAKEUP, dorm_static_info.LOTTERY_NUMS, amenities.F_KITCHEN, amenities.P_KITCHEN, amenities.P_BATHROOM
-        FROM dorm_static_info OUTER JOIN amenities
-        ON dorm_static_info.DORM = amenities.DORM
-        WHERE dorm_static_info.DORM = "${req.params.dorm}";`
-       
-	const result = await pool.query(query);
-	res.send(result[0])
+      let query = `SELECT dorm_static_info.DORM, dorm_static_info.WALKTHROUGH, dorm_static_info.SUITE,
+                          dorm_static_info.SINGLE_, dorm_static_info.DOUBLE_, dorm_static_info.TRIPLE_,
+                          dorm_static_info.CLASS_MAKEUP, dorm_static_info.LOTTERY_NUMS, amenities.F_KITCHEN,
+                          amenities.P_KITCHEN, amenities.P_BATHROOM
+                   FROM dorm_static_info RIGHT JOIN amenities
+                   ON dorm_static_info.DORM = amenities.DORM
+                   WHERE amenities.DORM = "${req.params.dorm}";`
+
+    	const result = await pool.query(query);
+    	res.send(result[0])
 })
 
 module.exports = router;
