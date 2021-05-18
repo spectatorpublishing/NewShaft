f941aaa751f6e105a94fffa5f3b892cce6abb183
nb2k.o <bnigel.2002@yahoo.com>
2021-05-18 23:37:25 +0200

    :grin: Tested code, need to verify field names

--- src/server/routes/addReview.js
+++ src/server/routes/addReview.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 var pool = require('../database');
@@ -6,23 +5,24 @@ var pool = require('../database');
 router.post('/', (req,res) =>{
 
    const review = req.body
-   var sql =  "INSERT INTO reviews (a,b,c,d) VALUES(?,?,?,?);";
+   var sql =  "INSERT INTO review_distinct (DORM,ROOM_NUM,YEAR,NUM_STARS,REVIEW_TXT) VALUES(?,?,?,?,?);";
 
    pool.query(
        sql,
        
        [
-           review.a,
-           review.b,
-           review.c,
-           review.d
+           review.DORM,
+           review.ROOM_NUM,
+           review.YEAR,
+           review.NUM_STARS,
+           review.REVIEW_TXT
        ],
 
 
 
    (err) =>{
        if(!err){
-        res.send(rows);
+        res.send("Good");
        }
 
        else{
