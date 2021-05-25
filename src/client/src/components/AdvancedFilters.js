f3c16b62b9047dfafff4eaef4cf86f646464ac5b
Yu-Chen Huang <yuchenxhuang@gmail.com>
2021-05-25 10:05:31 -0400

    changed style

--- src/client/src/components/AdvancedFilters.js
+++ src/client/src/components/AdvancedFilters.js
@@ -2,7 +2,43 @@ import React, { Component } from "react";
 import styled from 'styled-components';
 import RangeSlider from './RangeSlider';
 
+let Number = styled.div`
+    font-family: 'Raleway';
+    text-align: center;
+    font-size: 1.5rem;
+    vertical-align: middle;
+    margin: 0rem 1rem;
+`;
+
+let RowGZ = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    font-family: 'Raleway';
+    justify-content: center;
+    margin: 0rem 0 0 1rem;
+`;
+
+const ButtonGZ = styled.div`
+    cursor: pointer;
+    font-family: 'Raleway';
+    font-size:1.2rem;
+    color: black;
+    justify-content: center;
+    border-radius: 100%;
+    border: 1px solid rgb(98, 168, 229);
+    text-align: center;
+    padding: 0rem 0.45rem 0rem 0.45rem;
+    vertical-align: center;
+    color: rgb(98, 168, 229);
+    &:hover {
+        background: rgb(98, 168, 229);
+        color: white;
+    }
+`;
+
 let Input = styled.input`
+    margin-left: 1rem;
     border: 2px solid rgb(98, 168, 229);
     border-radius: 6px;
     height: 1rem;
@@ -21,12 +57,13 @@ const Filter = styled.div`
     justify-content: space-between;
     color: ${props => props.theme.black};
     font-size: 1.125rem;
-    margin-top: 0.75rem;
+    margin: 0.5rem;
     font-weight: 400;
 `
 
 const Section = styled.div`
-    color: ${props => props.theme.columbiaBlue};
+    max-width: ${props => props.large ? "50%" : "25%" };
+    color: ${props => props.theme.white};
     font-size: 1.25rem;
     margin: 1rem;
     font-weight: 400;
@@ -34,10 +71,14 @@ const Section = styled.div`
 `
 
 const Subsection = styled.div`
-    color: ${props => props.theme.columbiaBlue};
+    color: ${props => props.theme.black};
     font-size: 1.125rem;
     margin-top: 1rem;
     font-weight: 400;
+    border: 1px solid ${props => props.theme.white};
+    background-color: ${props => props.theme.white};
+    border-radius: 6px;
+    padding: 0.5rem;
 `
 
 const Amenities = styled.div`
@@ -48,29 +89,31 @@ const Amenities = styled.div`
 `
 
 const Box = styled.div`
-    display: flex column;
-    border: 1px solid ${props => props.theme.columbiaBlue};
+    display: flex;
+    padding-bottom: 1rem;
+    /*
+    border: 1px solid ${props => props.theme.white};
     margin: 1rem;
     padding: 1rem;
     width: 500px;
     border-radius: 6px;
+    */
 `
 
 const Row = styled.div`
     display: flex;
     justify-content: space-between;
-    color: ${props => props.theme.columbiaBlue};
+    color: ${props => props.theme.white};
 
 `
 
 const Column = styled.div`
     display: flex;
     flex-direction: column;
-    width: 45%;
 `
 
 let Exit = styled.div`
-    color: ${props => props.theme.columbiaBlue};
+    color: ${props => props.theme.white};
     cursor: pointer;
 `
 
@@ -127,7 +170,8 @@ export default class AdvancedFilters extends Component{
             FloorLounge: false,
             Print: false,
             BuildingLounge: false,
-            NoCarpet: false
+            NoCarpet: false,
+            PerLaundry: 1
 	    };
 
     }
@@ -135,9 +179,8 @@ export default class AdvancedFilters extends Component{
     render () {
         return(
             <Box>  
-                <Section> <Row> BATHROOMS <Exit> X </Exit> </Row>
-                    <Subsection> Bathroom Style 
-                        <Column>
+                <Section> BATHROOMS 
+                    <Subsection> 
                             <Filter> Private 
                                 <Input id="Private"   
                                        onClick = {null}  
@@ -145,33 +188,25 @@ export default class AdvancedFilters extends Component{
                                        checked = {null} 
                                        onChange = {null} /> 
                                 </Filter>
-                            <Filter> Hallway, single use 
+                            <Filter> Single use 
                                 <Input id="HallwaySingle"  
                                        onClick = {null}  
                                        type="checkbox" 
                                        checked = {null} 
                                        onChange = {null} /> 
                                 </Filter>
-                            <Filter> Hallway, multiple use 
+                            <Filter> Multiple use 
                                 <Input id="HallwayMultiple"  
                                        onClick = {null}  
                                        type="checkbox" 
                                        checked = {null} 
                                        onChange = {null} /> 
                                 </Filter>
-                        </Column>
-                    </Subsection>
-                    <Subsection> Average number of people per bathroom 
-                        <Slider><RangeSlider/></Slider>
                     </Subsection>
                 </Section>
 
-                <Section> LAUNDRY 
-                    <Subsection> Average number of people per machine </Subsection>
-                        <Slider><RangeSlider/></Slider>
-                </Section>
-
-                <Section> OTHER AMENITIES 
+                <Section large> AMENITIES 
+                <Subsection>
                     <Amenities> 
                         <Column>
                             <Filter> Kitchen, private 
@@ -181,13 +216,7 @@ export default class AdvancedFilters extends Component{
                                        checked = {null} 
                                        onChange = {null} /> 
                                 </Filter>
-                            <Filter> Practice room 
-                                <Input id="Practice"  
-                                       onClick = {null}  
-                                       type="checkbox" 
-                                       checked = {null} 
-                                       onChange = {null} /> 
-                                </Filter>
+
                             <Filter> Kitchen, shared 
                                 <Input id="KitchenShared"  
                                        onClick = {null}  
@@ -195,24 +224,8 @@ export default class AdvancedFilters extends Component{
                                        checked = {null} 
                                        onChange = {null} /> 
                                 </Filter>
-                            <Filter> Fitness room 
-                                <Input id="Fitness"  
-                                       onClick = {null}  
-                                       type="checkbox" 
-                                       checked = {null} 
-                                       onChange = {null} /> 
-                                </Filter>
-                        </Column>
-                        <Column>
                             <Filter> Floor lounge 
                                 <Input id="FloorLounge"   
-                                       onClick = {null}  
-                                       type="checkbox" 
-                                       checked = {null} 
-                                       onChange = {null} /> 
-                                </Filter>
-                            <Filter> Print station 
-                                <Input id="Print"  
                                        onClick = {null}  
                                        type="checkbox" 
                                        checked = {null} 
@@ -225,6 +238,32 @@ export default class AdvancedFilters extends Component{
                                        checked = {null} 
                                        onChange = {null} /> 
                                 </Filter>
+
+                        </Column>
+                        <Column>
+                            <Filter> Practice room 
+                                <Input id="Practice"  
+                                       onClick = {null}  
+                                       type="checkbox" 
+                                       checked = {null} 
+                                       onChange = {null} /> 
+                                </Filter>
+                            <Filter> Fitness room 
+                                <Input id="Fitness"  
+                                       onClick = {null}  
+                                       type="checkbox" 
+                                       checked = {null} 
+                                       onChange = {null} /> 
+                                </Filter>
+
+                            <Filter> Print station 
+                                <Input id="Print"  
+                                       onClick = {null}  
+                                       type="checkbox" 
+                                       checked = {null} 
+                                       onChange = {null} /> 
+                                </Filter>
+
                             <Filter> No carpet 
                                 <Input id="NoCarpet"   
                                        onClick = {null}  
@@ -234,10 +273,19 @@ export default class AdvancedFilters extends Component{
                                 </Filter>
                         </Column>
                     </Amenities>
+                </Subsection>
                 </Section>
-                <Search> 
-                    <DropButton>Search</DropButton>
-                </Search>
+
+                <Section> LAUNDRY 
+                    <Subsection> 
+                            <Filter>Average number of people per machine </Filter> 
+                                <RowGZ>
+                                    <ButtonGZ id = "-" onClick={this.onChange}>-</ButtonGZ>
+                                    <Number>{this.state.PerLaundry}</Number>
+                                    <ButtonGZ class="button" id = "+" onClick={this.onChange}>+</ButtonGZ>
+                                </RowGZ> 
+                    </Subsection>
+                </Section>
 
             </Box>
         );
