0c9140e0645a8e1e8dd7bde3793990082bd5fbb2
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-03-13 23:08:36 -0400

    updated queries

--- src/server/routes/getExploreInfo.js
+++ src/server/routes/getExploreInfo.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var pool = require('../database');
 
 router.get('/', async (req, res) => {
-	let query = `SELECT DORM, DESCRIPTION, COLLEGE, THUMBNAIL_IMAGE, LATITUDE, LONGITUDE, SINGLE_, DOUBLE_, TRIPLE_, CLASS_MAKEUP FROM dorm_static_info; `
+	let query = `SELECT DORM, DESCRIPTION, COLLEGE, THUMBNAIL_IMAGE, LATITUDE, LONGITUDE, SINGLE_, DOUBLE_, TRIPLE_, CLASS_MAKEUP, SUITE_ FROM dorm_static_info; `
 	const result = await pool.query(query);
 	res.send(result)
 })
