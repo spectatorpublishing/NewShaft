7ba4c1d5a8ec732b3cfa00d164d61e7b85e881bd
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-03-24 00:15:02 -0400

    Update FloorButton.js

--- src/client/src/components/FloorButton.js
+++ src/client/src/components/FloorButton.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 import Dropdown from 'react-bootstrap/Dropdown';
-import ChrisV from '../assets/chrisv_blue.svg';
-import { theme } from '../util/GlobalStyles.js';
 
 
 let NumberBtn = styled.button`
@@ -32,9 +30,6 @@ let NumberSelected = styled(NumberBtn)`
    font-family: Raleway, sans-serif;
    font-weight: 400;
    font-size: 1rem;
-   padding: 0.2rem 1rem;
-   margin: 0.2rem 0;
-   text-align: left;
    border: solid 1px ${props => props.theme.lightGray};
    color: ${props => props.theme.white};    
    background-color: ${props => props.theme.columbiaBlue};
@@ -46,13 +41,6 @@ let NumberSelected = styled(NumberBtn)`
     &:hover {
         background-color: ${props => props.theme.lightGray};
     }
-`
-let ButtonTopText = styled.p`
-    color: 	${props => props.theme.columbiaBlue};
-    font-weight: bold;
-    margin-top: 1rem;
-    margin-bottom:0.5rem;
-    text-align: left;
 `
 
 let FloorButtonWrapper = styled.div`
@@ -73,39 +61,6 @@ let Buttons = styled.div`
         flex-direction: row;
     }
 `
-
-let StyledToggle = styled(Dropdown.Toggle)`
-   background-color: white;
-   padding: 0.8rem 0.0rem 0.8rem 0.4rem;
-   width: 100%
-   border: none;
-   border-radius: 10px;
-`;
-
-let Items = styled(Dropdown.Item)`
-   background-color: white;
-   width: 100%;
-   
-`;
-
-let Menu = styled(Dropdown.Menu)`
-   background-color: white;
-   width: 90%;
-   box-shadow: 2px 2px 2px lightgray;
-   
-`;
-
-let StyledDropdown = styled(Dropdown)`
-   background-color: white;
-   width: 100%;
-   border-radius: 10px;
-`;
-
-let V = styled.img`
-   display: block;
-   margin-left: auto;
-`;
-
 
 export default class FloorButton extends Component {
     constructor(props) {
