ee69398c9678e921134ef3858233b8ec3787e71b
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-02-26 21:50:40 -0500

    removed more unecessary files and updated the server index.js

--- index.js
+++ index.js
@@ -5,21 +5,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 // require('dotenv').config();
 
-
 app.use(cors())
 
 const mountRoutes = require('./server/routes')
-
-const port = process.env.PORT || 8080;
-const server = app.listen(port, () => console.log(`Connected on port ${port}`))
 
 app.use(express.static('dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 mountRoutes(app)
-
 
 const path = require('path')
 // Serve static files from the React frontend app
@@ -28,5 +22,8 @@ app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/./client/build/index.html'))
 })
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => console.log(`Connected on port ${port}`))
 
 module.exports = server;
