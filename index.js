cdb8d4cf8ce0c603b47930f01197856e9d4a1352
Cindy Zhu <zhucindy1234567890@gmail.com>
2023-03-02 16:31:33 -0500

    :rocket: node version + updating port to 8080 for server

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/./client/build/index.html'))
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => console.log(`Connected on port ${port}`))
 
 module.exports = server;
