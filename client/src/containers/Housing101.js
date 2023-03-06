28ab27099d578d2f8490b35f0b07f926a1067b4a
Francesca Cavuoti <fc2637@columbia.edu>
2023-03-05 19:40:19 -0500

    removed reviews section from housing101

--- client/src/containers/Housing101.js
+++ client/src/containers/Housing101.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../css/Housing101.css';
 import NewProcess from "../components/NewProcess.js";
 import SpecialHousing from "../components/SpecialHousing.js";
 import UsingtheSHAFT from "../components/UsingtheSHAFT.js";
 
-const Housing101 = () => {
+const Housing101 = (props) => {
     const [currentPage, setPage] = useState("/")
 
     const CurrentPage = () => {
+
         if (currentPage === "specialHousing") {
             return <SpecialHousing />
         } else if (currentPage === "newProcess") {
@@ -20,12 +21,12 @@ const Housing101 = () => {
     return (
         <div>
             <div class="header">
-                <div class="li"><div onClick={() => setPage("")} className={(currentPage === "" ? "active" : "")}>using the<strong>SHAFT</strong></div></div>
-                <div class="li"><div onClick={() => setPage("newProcess")} className={(currentPage === "newProcess" ? "active" : "")}>the new process</div></div>
-                <div class="li"><div onClick={() => setPage("specialHousing")} className={(currentPage === "specialHousing" ? "active" : "")}>special housing</div></div>
+                <div className="li"><div onClick={() => setPage("/")} className={(currentPage === "/" ? "active" : "")}>using the<strong>SHAFT</strong></div></div>
+                <div className="li"><div onClick={() => setPage("newProcess")} className={(currentPage === "newProcess" ? "active" : "")}>the new process</div></div>
+                <div className="li"><div onClick={() => setPage("specialHousing")} className={(currentPage === "specialHousing" ? "active" : "")}>special housing</div></div>
             </div>
             <div className="content">
-                <CurrentPage />
+            <CurrentPage />
             </div>
         </div>
     )
