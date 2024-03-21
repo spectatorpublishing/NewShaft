cbf59de566fcf5921dccb2c71c8b832ce3fcc2d9
Darien Moment <dmoment18@gmail.com>
2024-03-21 12:58:15 -0400

    Change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
 
 app.use(cors())
 
